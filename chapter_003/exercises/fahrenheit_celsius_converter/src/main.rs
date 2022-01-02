c9650b2d965b0aed2a6e23008c1581324e25ac9b
akirilyuk <a.kirilyuk@cognigy.com>
2022-01-02 09:03:34 +0100

    simplify input number fn

--- chapter_003/exercises/fahrenheit_celsius_converter/src/main.rs
+++ chapter_003/exercises/fahrenheit_celsius_converter/src/main.rs
@@ -11,26 +11,20 @@ fn fahrenheit_to_celsius(f: f32) -> f32{
 }
 
 fn read_input_number(msg: &str) -> f32 {
-    let input_verified: f32;
     loop{
         let mut input = String::new();
         println!("{}", msg);
         io::stdin()
             .read_line(&mut input)
             .expect("Failed to read line!");
-        let input: f32 = match input.trim().parse(){
+        match input.trim().parse(){
             Ok(num) => {
-                num
+                break num;
             },
             Err(err) => {
                 println!("Encountered an error: {}", err);
                 continue;
             }
-        };
-        // println!("This one is not reached on error because of the continue");
-        input_verified = input;
-        break;
+        }
     }
-
-    input_verified
 }
